e4cddd2699202b26dd30a9ba8b085f891bacf1e8
denobytes <106289638+denobytes@users.noreply.github.com>
2023-05-12 17:28:05 -0500

    initial commit
    * use current latest release for svelte (3.59.1)

---
+++ README.md
@@ -0,0 +1,26 @@
+
+[![Latest version](https://deno.land/badge/svcc/version)](https://deno.land/x/svcc)
+
+**svcc** is a cli tool to compile svelte files
+
+## usage
+
+to see help:
+
+```sh
+deno run --allow-net 'https://deno.land/x/svcc/cli.ts' --help
+```
+
+example:
+
+```
+deno run --allow-net 'https://deno.land/x/svcc/cli.ts' Hello.svelte
+...
+...
+```
+
+## license
+
+Copyright 2023 **denobytes**.\
+See [LICENCE](LICENSE) file to get more infomation.
+
---
+++ cli.ts
@@ -0,0 +1,127 @@
+import { parse as argparse } from "https://deno.land/std@0.181.0/flags/mod.ts";
+import { compile } from "./compiler.mjs";
+
+const args = argparse(Deno.args, {
+  boolean: [
+    // instructions for this script
+    "help",
+
+    // output css
+    "s",
+    "css",
+
+    // output js
+    "j",
+    "js",
+  ],
+  string: [
+    // compiler options
+    "options",
+
+    // output type
+    "type",
+    "t",
+  ],
+});
+
+const commandName = `svcc`;
+
+const usageMessage = `
+Usage: ${commandName} [OPTIONS] [-o OUTPUT-FILE] [SVELTE-FILE]
+Compile a svelte file to js or css output
+
+Options:
+  --help              Show this help message
+
+  -j, --js            Shorthand for --type js
+  -s, --css           Shorthand for --type css
+  -o, --output NAME   Write output to NAME
+  -t, --type TYPE     Output type (ast, css, js)
+  --options JSON      Options (JSON) for svelte compiler
+
+  Examples:
+  ${commandName} -j App.svelte
+  ${commandName} -s App.svelte
+  ${commandName} -t js App.svelte
+  ${commandName} -o a.out App.svelte
+  ${commandName} -t js -o a.out App.svelte
+  cat App.svelte | ${commandName}
+`;
+
+// parse args
+const help = args.help;
+const readStdin = args._.length == 0;
+const outputFilename = args.output || args.o;
+const compileOptions = args.options
+const outputType = args.type || args.t;
+
+let outputJS = args.js || args.j;
+let outputCSS = args.css || args.s;
+let outputAST = false;
+let compileStr = "";
+let opts = {};
+
+if (help) {
+  console.log(usageMessage);
+  Deno.exit();
+}
+
+if (compileOptions) {
+  console.log(compileOptions);
+	opts = JSON.parse(compileOptions);
+  console.log(opts);
+}
+
+if (readStdin) {
+  const decoder = new TextDecoder();
+  for await (const chunk of Deno.stdin.readable) {
+    const textChunk = decoder.decode(chunk);
+    compileStr += textChunk;
+  }
+} else {
+  const inputFilename = args._.at(0);
+
+  compileStr = await Deno.readTextFile(inputFilename);
+}
+
+let compiledResult = compile(compileStr,opts);
+let result = "";
+
+// only one output type
+if (outputType) {
+  if (outputJS || outputCSS) {
+    console.log(usageMessage);
+    Deno.exit();
+  }
+  if (outputType == "js") {
+    outputJS = true;
+  } else if (outputType == "css") {
+    outputCSS = true;
+  } else if (outputType == "ast") {
+    outputAST = true;
+  } else {
+    console.log(usageMessage);
+    Deno.exit();
+  }
+}
+
+if (outputJS) {
+  result = compiledResult.js.code;
+} else if (outputCSS) {
+  result = compiledResult.css.code;
+} else {
+  result = compiledResult;
+}
+
+if (outputFilename) {
+  try {
+    if (outputAST) {
+      result = JSON.stringify(result);
+    }
+    Deno.writeTextFileSync(outputFilename, result);
+  } catch (e) {
+    console.log(result);
+  }
+} else {
+  console.log(result);
+}
